9219c824455dff3edf67de5403f82af8af85c648
WisamAbbasi <39978331+WisamAbbasi@users.noreply.github.com>
2022-07-17 05:29:49 +0200

    Modifying code files

--- deep_wavFile.py
+++ deep_wavFile.py
@@ -1,91 +1,80 @@
-import wave
-
-import numpy as np
-import spacy
 from deepspeech import Model
+import numpy as np
+import os
+import wave
+import spacy
+
+
+# Import the required module for text 
+# to speech conversion
+from gtts import gTTS
+  
+# This module is imported so that we can 
+# play the converted audio
+import os
+
 
 nlp = spacy.load("en_core_web_sm")
 
-entity_types = [
-    "PERSON",
-    "NORP",
-    "FAC",
-    "ORG",
-    "GPE",
-    "LOC",
-    "PRODUCT",
-    "EVENT",
-    "WORK_OF_ART",
-    "LAW",
-    "LANGUAGE",
-    "DATE",
-    "TIME",
-    "PERCENT",
-    "MONEY",
-    "QUANTITY",
-    "ORDINAL",
-    "CARDINAL",
-]
-entity_desc = [
-    "People, including fictional.",
-    "Nationalities or religious or political groups.",
-    "Buildings, airports, highways, bridges, etc.",
-    "Companies, agencies, institutions, etc.",
-    "Countries, cities, states.",
-    "Non-GPE locations, mountain ranges, bodies of water.",
-    "Objects, vehicles, foods, etc. (Not services.)",
-    "Named hurricanes, battles, wars, sports events, etc.",
-    "Titles of books, songs, etc.",
-    "Named documents made into laws.",
-    "Any named language.",
-    "Absolute or relative dates or periods.",
-    "Times smaller than a day.",
-    'Percentage, including "%".',
-    "Monetary values, including unit.",
-    "Measurements, as of weight or distance.",
-    '"first", "second", etc.',
-    "Numerals that do not fall under another type.",
-]
+entity_types = ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LAW', 'LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
+entity_desc = ['People, including fictional.', 'Nationalities or religious or political groups.', 'Buildings, airports, highways, bridges, etc.', 'Companies, agencies, institutions, etc.', 'Countries, cities, states.', 'Non-GPE locations, mountain ranges, bodies of water.', 'Objects, vehicles, foods, etc. (Not services.)', 'Named hurricanes, battles, wars, sports events, etc.', 'Titles of books, songs, etc.', 'Named documents made into laws.', 'Any named language.', 'Absolute or relative dates or periods.', 'Times smaller than a day.', 'Percentage, including "%".', 'Monetary values, including unit.', 'Measurements, as of weight or distance.', '"first", "second", etc.', 'Numerals that do not fall under another type.']
 
 for (item, desc) in zip(entity_types, entity_desc):
     print(item, ": ", desc)
 
-model_file_path = "deepspeech-0.9.3-models.pbmm"
-lm_file_path = "deepspeech-0.9.3-models.scorer"
-beam_width = 500  # how many different sequences need to be evaluated
-lm_alpha = 0.93  # optimum parameter suggested in the repo
+model_file_path = 'deepspeech-0.9.3-models.pbmm'
+lm_file_path = 'deepspeech-0.9.3-models.scorer'
+beam_width = 500 #how many different word sequences that needs to be evaluated by acoustic model 
+lm_alpha = 0.93 #optimum parameter suggested in the repo
 lm_beta = 1.18
 
 model = Model(model_file_path)
 model.enableExternalScorer(lm_file_path)
 
-
 def read_wav_file(filename):
-    with wave.open(filename, "rb") as w:
-        rate = w.getframerate()
-        frames = w.getnframes()
-        buffer = w.readframes(frames)
-        print(rate)
-        print(frames)
-    return buffer, rate
-
+  with wave.open(filename,'rb') as w:
+    rate = w.getframerate()
+    frames = w.getnframes()
+    buffer = w.readframes(frames)
+    print(rate)
+    print(frames)
+  return buffer, rate
 
 def transcribe(audio_file):
-    buffer, rate = read_wav_file(audio_file)
-    data = np.frombuffer(buffer, dtype=np.int16)
-    return model.stt(data)
+  buffer, rate = read_wav_file(audio_file)
+  data = np.frombuffer(buffer,dtype=np.int16)
+  return model.stt(data)
 
-
-text = transcribe("sample2.wav")
+text=transcribe('sample2.wav')
 print("Original Text:", text)
 
 doc = nlp(text)
-text2 = text
+text2=text
 
 print("------Entities------")
 for ent in doc.ents:
     print(ent.text, ent.start, ent.end, ent.start_char, ent.end_char, ent.label_)
-    length = ent.end_char - ent.start_char
-    text2 = text2[: ent.start_char] + "x" * length + text2[ent.end_char:]
+    length=ent.end_char - ent.start_char
+    text2 = text2[:ent.start_char] + "x"*length + text2[ent.end_char:]
 
-print("Modified Text:", text2)
+Final_Text=""
+
+for item in text2.split():
+    if item.count('xx') >= 1:
+        # print(item, item.count('xx'))
+        Final_Text = Final_Text + 'Privat Data '
+    else:
+        # print(item, item.count('xx'))
+        Final_Text = Final_Text + item + ' '
+        
+print("Modified Text:", Final_Text)
+
+# Language in which you want to convert
+language = 'en'
+
+myobj = gTTS(text=Final_Text, lang=language, slow=False)
+
+myobj.save("Output_Audio.mp3")
+  
+# Playing the converted file
+os.system("mpg321 Output_Audio.mp3")
--- record_audio.py
+++ record_audio.py
@@ -1,47 +1,45 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 07:24:23 2022
 
-import copy
-import wave
+@author: wisam
+"""
 from array import array
 from struct import pack
 from sys import byteorder
-
+import copy
 import pyaudio
+import wave
 
 THRESHOLD = 500  # audio levels not normalised.
 CHUNK_SIZE = 1024
 SILENT_CHUNKS = 3 * 44100 / 1024  # about 3sec
 FORMAT = pyaudio.paInt16
-FRAME_MAX_VALUE = 2**15 - 1
+FRAME_MAX_VALUE = 2 ** 15 - 1
 NORMALIZE_MINUS_ONE_dB = 10 ** (-1.0 / 20)
 RATE = 44100
 CHANNELS = 1
 TRIM_APPEND = RATE / 4
 
-
 def is_silent(data_chunk):
     """Returns 'True' if below the 'silent' threshold"""
     return max(data_chunk) < THRESHOLD
 
-
 def normalize(data_all):
     """Amplify the volume out to max -1dB"""
     # MAXIMUM = 16384
-    normalize_factor = float(NORMALIZE_MINUS_ONE_dB * FRAME_MAX_VALUE) / max(
-        abs(i) for i in data_all
-    )
+    normalize_factor = (float(NORMALIZE_MINUS_ONE_dB * FRAME_MAX_VALUE)
+                        / max(abs(i) for i in data_all))
 
-    r = array("h")
+    r = array('h')
     for i in data_all:
         r.append(int(i * normalize_factor))
     return r
-
 
 def trim(data_all):
     _from = 0
     _to = len(data_all) - 1
     for i, b in enumerate(data_all):
-
         if abs(b) > THRESHOLD:
             _from = max(0, i - TRIM_APPEND)
             break
@@ -51,31 +49,23 @@ def trim(data_all):
             _to = min(len(data_all) - 1, len(data_all) - 1 - i + TRIM_APPEND)
             break
 
-    return copy.deepcopy(data_all[int(_from): (int(_to) + 1)])
-
+    return copy.deepcopy(data_all[int(_from):(int(_to) + 1)])
 
 def record():
-    """Record a word or words from the microphone and
+    """Record a word or words from the microphone and 
     return the data as an array of signed shorts."""
 
     p = pyaudio.PyAudio()
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        output=True,
-        frames_per_buffer=CHUNK_SIZE,
-    )
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK_SIZE)
 
     silent_chunks = 0
     audio_started = False
-    data_all = array("h")
+    data_all = array('h')
 
     while True:
         # little endian, signed short
-        data_chunk = array("h", stream.read(CHUNK_SIZE))
-        if byteorder == "big":
+        data_chunk = array('h', stream.read(CHUNK_SIZE))
+        if byteorder == 'big':
             data_chunk.byteswap()
         data_all.extend(data_chunk)
 
@@ -86,37 +76,33 @@ def record():
                 silent_chunks += 1
                 if silent_chunks > SILENT_CHUNKS:
                     break
-            else:
+            else: 
                 silent_chunks = 0
         elif not silent:
-            audio_started = True
+            audio_started = True              
 
     sample_width = p.get_sample_size(FORMAT)
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    data_all = trim(
-        data_all
-    )  # we trim before normalize
+    data_all = trim(data_all)  # we trim before normalize as threshhold applies to un-normalized wave (as well as is_silent() function)
     data_all = normalize(data_all)
     return sample_width, data_all
-
 
 def record_to_file(path):
     "Records from the microphone and outputs the resulting data to 'path'"
     sample_width, data = record()
-    data = pack("<" + ("h" * len(data)), *data)
+    data = pack('<' + ('h' * len(data)), *data)
 
-    wave_file = wave.open(path, "wb")
+    wave_file = wave.open(path, 'wb')
     wave_file.setnchannels(CHANNELS)
     wave_file.setsampwidth(sample_width)
     wave_file.setframerate(RATE)
     wave_file.writeframes(data)
     wave_file.close()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     print("Wait in silence to begin recording; wait in silence to terminate")
-    record_to_file("demo.wav")
+    record_to_file('demo.wav')
     print("done - result written to demo.wav")
